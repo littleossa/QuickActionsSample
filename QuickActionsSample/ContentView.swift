9fca1520038f2c02e24adccf8a442b11c13afd8f
littleossa <littleossa@gmail.com>
2022-08-15 17:38:33 +0900

    Initial Commit

---
+++ QuickActionsSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickActionsSample
+//
+//  Created by 平岡修 on 2022/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickActionsSample/QuickActionsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickActionsSampleApp.swift
+//  QuickActionsSample
+//
+//  Created by 平岡修 on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct QuickActionsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
